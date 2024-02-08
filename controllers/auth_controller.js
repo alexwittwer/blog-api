13e416b667fc44ecbcfb15ae2ec89b76a2155b09
alexwittwer <wittweral@gmail.com>
2024-02-08 18:49:15 -0500

    add isAdmin

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -12,7 +12,7 @@ exports.login = asyncHandler(async (req, res) => {
   let { email, password } = req.body;
 
   try {
-    const user = await UserAuth.findOne({ email });
+    const user = await UserAuth.findOne({ email }).populate().exec();
     const match = await bcrypt.compare(password, user.password);
 
     if (!user) return res.status(404).send();
